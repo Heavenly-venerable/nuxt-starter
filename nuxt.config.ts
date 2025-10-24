b4373daa56e206f6a18950384bbd27c8883576bb
Heavenly-venerable <ricrichar94@gmail.com>
2025-10-24 07:42:10 +0700

    chore: setup primevue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,18 @@
+import Aura from '@primeuix/themes/aura';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: false },
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
+  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@primevue/nuxt-module'],
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
+  },
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura
+      }
+    }
   },
 })
--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="pt-24">
-    Hello World!
+  <div class="min-h-svh w-full flex justify-center items-center gap-4">
+    <h1 class="text-4xl font-bold text-gray-800">Hello Primevue</h1>
+    <Button label="Button" />
   </div>
 </template>
 
